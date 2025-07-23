5b611bfc17a262968135fa3125663ee930b7d82c
ahmed khattab <ahm56523@gmail.com>
2025-07-23 19:42:15 +0200

    Refactoring sensor_data_log

--- Core/Inc/log_sensor_data.h
+++ Core/Inc/log_sensor_data.h
@@ -17,6 +17,32 @@ typedef enum {
     LOG_FILE_OPEN_ERROR
 } LogStatus;
 
+// The main struct for the SD Card Logger
+typedef struct {
+    // FatFs handles
+    FATFS fs;
+    FIL fil;
+    FATFS *pfs;
+    
+    // Buffers for data handling
+    char log_buffer[LOG_BUFFER_SIZE];
+    char uart_buffer[UART_BUFFER_SIZE];
+    char sensor_data_buffer[SENSOR_DATA_BUFFER_SIZE];
+    
+    // File I/O tracking
+    UINT bytes_written;
+    UINT bytes_read;
+    
+    // SD Card Information
+    DWORD free_clusters;
+    uint32_t total_space_kb;
+    uint32_t free_space_kb;
+    
+    // Hardware Handles (to be passed in during initialization)
+    UART_HandleTypeDef *uart_handle;
+    // RTC_HandleTypeDef *rtc_handle; // Optional
+} SD_Card_Logger;
+
 // Sensor data structure
 typedef struct {
     float temperature;
@@ -32,24 +58,24 @@ typedef struct {
 #define DATA_FILENAME          "sensor_data.csv"
 #define LOG_INTERVAL_MS        5000
 
-// Function prototypes
-LogStatus log_sensor_init(void);
-LogStatus log_sensor_data(SensorData *data);
-LogStatus log_sensor_data_with_rtc(SensorData *data);
-LogStatus create_data_file_header(void);
-LogStatus display_last_entries(uint8_t num_entries);
-LogStatus clear_log_file(void);
-LogStatus get_sd_card_info(void);
+/ Function declarations
+LogStatus log_sensor_init(SD_Card_Logger *logger, UART_HandleTypeDef *uart_handle);
+LogStatus get_sd_card_info(SD_Card_Logger *logger);
+LogStatus create_data_file_header(SD_Card_Logger *logger);
+LogStatus log_sensor_data(SD_Card_Logger *logger, const SensorData *data);
+LogStatus display_last_entries(SD_Card_Logger *logger, uint8_t num_entries);
+LogStatus clear_log_file(SD_Card_Logger *logger);
+LogStatus check_sd_card_status(SD_Card_Logger *logger);
+
+// File operation helpers
+LogStatus open_data_file_append(SD_Card_Logger *logger);
+LogStatus open_data_file_read(SD_Card_Logger *logger);
+LogStatus close_data_file(SD_Card_Logger *logger);
 
 // Utility functions
-void send_uart_message(const char *message);
-void clear_log_buffer(void);
+void send_uart_message(SD_Card_Logger *logger, const char *message);
+void clear_log_buffer(SD_Card_Logger *logger);
 uint32_t get_buffer_size(const char *buf);
-LogStatus check_sd_card_status(void);
-
-// File operations
-LogStatus open_data_file_append(void);
-LogStatus open_data_file_read(void);
-LogStatus close_data_file(void);
+LogStatus check_sd_card_status(SD_Card_Logger *logger);
 
 #endif // LOG_SENSOR_DATA_H
--- Core/Src/log_sensor_data.c
+++ Core/Src/log_sensor_data.c
@@ -1,79 +1,87 @@
 #include "log_sensor_data.h"
 
 // Global variables for logging
-static FATFS fs;
-static FIL fil;
-static FRESULT fresult;
-static char log_buffer[LOG_BUFFER_SIZE];
-static char uart_buffer[UART_BUFFER_SIZE];
-static char sensor_data_buffer[SENSOR_DATA_BUFFER_SIZE];
-static UINT bytes_written, bytes_read;
-static DWORD free_clusters;
-static FATFS *pfs;
-static uint32_t total_space, free_space;
 
-// External UART handle (should be defined in main.c)
-extern UART_HandleTypeDef huart1;
+static FRESULT fresult;
+
+
+
 
 // Optional RTC handle (uncomment if using RTC)
 // extern RTC_HandleTypeDef hrtc;
 
-// Initialize SD card and logging system
-LogStatus log_sensor_init(void)
+LogStatus log_sensor_init(SD_Card_Logger *logger, UART_HandleTypeDef *uart_handle)
 {
+    if (!logger || !uart_handle) {
+        return LOG_SD_MOUNT_ERROR;
+    }
+    
+    // Initialize logger structure
+    memset(logger, 0, sizeof(SD_Card_Logger));
+    logger->uart_handle = uart_handle;
+    
     HAL_Delay(500);
 
     // Mount SD card
-    fresult = f_mount(&fs, "/", 1);
+    FRESULT fresult = f_mount(&logger->fs, "/", 1);
     if (fresult != FR_OK) {
-        send_uart_message("ERROR!!! in mounting SD CARD...\r\n");
+        send_uart_message(logger, "ERROR!!! in mounting SD CARD...\r\n");
         return LOG_SD_MOUNT_ERROR;
     }
     else {
-        send_uart_message("SD CARD mounted successfully...\r\n");
+        send_uart_message(logger, "SD CARD mounted successfully...\r\n");
     }
 
     // Get SD card information
-    if (get_sd_card_info() != LOG_OK) {
-        send_uart_message("Warning: Could not get SD card info\r\n");
+    if (get_sd_card_info(logger) != LOG_OK) {
+        send_uart_message(logger, "Warning: Could not get SD card info\r\n");
     }
 
     // Create data file header if needed
-    if (create_data_file_header() != LOG_OK) {
-        send_uart_message("ERROR!!! Could not create data file header\r\n");
+    if (create_data_file_header(logger) != LOG_OK) {
+        send_uart_message(logger, "ERROR!!! Could not create data file header\r\n");
         return LOG_FILE_CREATE_ERROR;
     }
 
-    send_uart_message("Sensor data logging system initialized\r\n");
+    send_uart_message(logger, "Sensor data logging system initialized\r\n");
     return LOG_OK;
 }
 
 // Get SD card capacity information
-LogStatus get_sd_card_info(void)
+LogStatus get_sd_card_info(SD_Card_Logger *logger)
 {
+    if (!logger) {
+        return LOG_SD_MOUNT_ERROR;
+    }
+
     fresult = f_getfree("", &free_clusters, &pfs);
     if (fresult != FR_OK) {
         return LOG_SD_MOUNT_ERROR;
     }
 
-    total_space = (uint32_t)((pfs->n_fatent - 2) * pfs->csize * 0.5);
-    sprintf(log_buffer, "SD CARD Total Size: %lu KB\r\n", total_space);
-    send_uart_message(log_buffer);
-    clear_log_buffer();
+    logger->total_space_kb = (uint32_t)((logger->pfs->n_fatent - 2) * logger->pfs->csize * 0.5);
+    sprintf(logger->log_buffer, "SD CARD Total Size: %lu KB\r\n", logger->total_space_kb);
+    send_uart_message(logger, logger->log_buffer);
+    clear_log_buffer(logger);
 
-    free_space = (uint32_t)(free_clusters * pfs->csize * 0.5);
-    sprintf(log_buffer, "SD CARD Free Space: %lu KB\r\n", free_space);
-    send_uart_message(log_buffer);
-    clear_log_buffer();
+
+    logger->free_space_kb = (uint32_t)(logger->free_clusters * logger->pfs->csize * 0.5);
+    sprintf(logger->log_buffer, "SD CARD Free Space: %lu KB\r\n", logger->free_space_kb);
+    send_uart_message(logger, logger->log_buffer);
+    clear_log_buffer(logger);
 
     return LOG_OK;
 }
 
 // Create CSV header if file doesn't exist
-LogStatus create_data_file_header(void)
+LogStatus create_data_file_header(SD_Card_Logger *logger)
 {
+    if (!logger) {
+        return LOG_FILE_CREATE_ERROR;
+    }
+
     // Check if data file exists
-    fresult = f_open(&fil, DATA_FILENAME, FA_READ);
+    fresult = f_open(&logger->fil, DATA_FILENAME, FA_READ);
 
     if (fresult == FR_NO_FILE) {
         // File doesn't exist, create it with header
@@ -83,21 +91,21 @@ LogStatus create_data_file_header(void)
         }
 
         // Write CSV header
-        strcpy(log_buffer, "Timestamp,Temperature(C),Humidity(%),Pressure(hPa)\r\n");
-        fresult = f_write(&fil, log_buffer, strlen(log_buffer), &bytes_written);
+        strcpy(logger->log_buffer, "Timestamp,Temperature(C),Humidity(%),Pressure(hPa)\r\n");
+        fresult = f_write(&logger->fil, logger->log_buffer, strlen(logger->log_buffer), &logger->bytes_written);
 
         if (fresult != FR_OK) {
-            f_close(&fil);
+            f_close(&logger->fil);
             return LOG_FILE_WRITE_ERROR;
         }
 
-        f_close(&fil);
-        send_uart_message("New data file created with header\r\n");
+        f_close(&logger->fil);
+        send_uart_message(logger, "New data file created with header\r\n");
     }
     else if (fresult == FR_OK) {
         // File exists, just close it
-        f_close(&fil);
-        send_uart_message("Existing data file found\r\n");
+        f_close(&logger->fil);
+        send_uart_message(logger, "Existing data file found\r\n");
     }
     else {
         return LOG_FILE_OPEN_ERROR;
@@ -112,101 +120,113 @@ LogStatus create_data_file_header(void)
 
 
 // Log sensor data to SD card
-LogStatus log_sensor_data(SensorData *data)
+LogStatus log_sensor_data(SD_Card_Logger *logger, const SensorData *data)
 {
+    if (!logger || !data) {
+        return LOG_FILE_WRITE_ERROR;
+    }
+
     // Open file in append mode
-    fresult = f_open(&fil, DATA_FILENAME, FA_OPEN_ALWAYS | FA_WRITE);
+    fresult = f_open(&logger->fil, DATA_FILENAME, FA_OPEN_ALWAYS | FA_WRITE);
 
     if (fresult != FR_OK) {
-        send_uart_message("Error opening data file\r\n");
+        send_uart_message(logger, "Error opening data file\r\n");
         return LOG_FILE_OPEN_ERROR;
     }
 
     // Move to end of file for appending
-    fresult = f_lseek(&fil, f_size(&fil));
+    fresult = f_lseek(&logger->fil, f_size(&logger->fil));
     if (fresult != FR_OK) {
-        f_close(&fil);
+        f_close(&logger->fil);
         return LOG_FILE_WRITE_ERROR;
     }
 
     // Format sensor data as CSV
-    sprintf(sensor_data_buffer, "%lu,%.2f,%.2f,%.2f\r\n",
+    sprintf(logger->sensor_data_buffer, "%lu,%.2f,%.2f,%.2f\r\n",
             data->timestamp, data->temperature, data->humidity, data->pressure);
 
     // Write data to file
-    fresult = f_write(&fil, sensor_data_buffer, strlen(sensor_data_buffer), &bytes_written);
+    fresult = f_write(&logger->fil, logger->sensor_data_buffer, strlen(logger->sensor_data_buffer), &logger->bytes_written);
 
     if (fresult != FR_OK) {
-        f_close(&fil);
-        send_uart_message("Error writing to SD card\r\n");
+        f_close(&logger->fil);
+        send_uart_message(logger, "Error writing to SD card\r\n");
         return LOG_FILE_WRITE_ERROR;
     }
 
     // Close file
-    f_close(&fil);
+    f_close(&logger->fil);
 
-    send_uart_message("Data logged to SD card successfully\r\n");
+    send_uart_message(logger, "Data logged to SD card successfully\r\n");
     return LOG_OK;
 }
 
 
 // Display last N entries from log file
-LogStatus display_last_entries(uint8_t num_entries)
+LogStatus display_last_entries(SD_Card_Logger *logger, uint8_t num_entries)
 {
+    if (!logger) {
+        return LOG_FILE_READ_ERROR;
+    }
+    
     char line[256];
     uint32_t file_size;
     uint32_t line_count = 0;
 
     // Open file for reading
-    fresult = f_open(&fil, DATA_FILENAME, FA_READ);
+    fresult = f_open(&logger->fil, DATA_FILENAME, FA_READ);
 
     if (fresult != FR_OK) {
-        send_uart_message("Error reading data file\r\n");
+        send_uart_message(logger, "Error reading data file\r\n");
         return LOG_FILE_READ_ERROR;
     }
 
-    file_size = f_size(&fil);
+    file_size = f_size(&logger->fil);
 
     // Read file line by line to count total lines
-    while (f_gets(line, sizeof(line), &fil)) {
+    while (f_gets(line, sizeof(line), &logger->fil)) {
         line_count++;
     }
 
     // Go back to beginning
-    f_lseek(&fil, 0);
+    f_lseek(&logger->fil, 0);
 
     // Skip header and older entries
     uint32_t skip_lines = (line_count > num_entries + 1) ? (line_count - num_entries) : 1;
 
     for (uint32_t i = 0; i < skip_lines; i++) {
-        f_gets(line, sizeof(line), &fil);
+        f_gets(line, sizeof(line), &logger->fil);
     }
 
     // Display remaining entries
-    send_uart_message("Last sensor readings:\r\n");
-    while (f_gets(line, sizeof(line), &fil)) {
-        send_uart_message(line);
+    send_uart_message(logger, "Last sensor readings:\r\n");
+    while (f_gets(line, sizeof(line), &logger->fil)) {
+        send_uart_message(logger, line);
     }
 
-    f_close(&fil);
+    f_close(&logger->fil);
     return LOG_OK;
 }
 
 // Clear all logged data
-LogStatus clear_log_file(void)
+LogStatus clear_log_file(SD_Card_Logger *logger)
 {
+    if (!logger) {
+        return LOG_FILE_OPEN_ERROR;
+    }
+    
     // Delete existing file
-    fresult = f_unlink(DATA_FILENAME);
+    FRESULT fresult = f_unlink(DATA_FILENAME);
 
     if (fresult != FR_OK) {
-        send_uart_message("Error clearing log file\r\n");
+        send_uart_message(logger, "Error clearing log file\r\n");
         return LOG_FILE_OPEN_ERROR;
     }
 
-    send_uart_message("Log file cleared successfully\r\n");
+    send_uart_message(logger, "Log file cleared successfully\r\n");
 
     // Recreate file with header
-    if (create_data_file_header() != LOG_OK) {
+    if (create_data_file_header(logger) != LOG_OK) {
         return LOG_FILE_CREATE_ERROR;
     }
 
@@ -214,42 +234,54 @@ LogStatus clear_log_file(void)
 }
 
 // Check SD card status
-LogStatus check_sd_card_status(void)
+LogStatus check_sd_card_status(SD_Card_Logger *logger)
 {
+    if (!logger) {
+        return LOG_SD_MOUNT_ERROR;
+    }
+    
     // Try to open a test file
-    fresult = f_open(&fil, "test.tmp", FA_CREATE_ALWAYS | FA_WRITE);
+    fresult = f_open(&logger->fil, "test.tmp", FA_CREATE_ALWAYS | FA_WRITE);
 
     if (fresult != FR_OK) {
         return LOG_SD_MOUNT_ERROR;
     }
 
-    f_close(&fil);
+    f_close(&logger->fil);
     f_unlink("test.tmp");
 
     return LOG_OK;
 }
 
 // File operation helpers
-LogStatus open_data_file_append(void)
+LogStatus open_data_file_append(SD_Card_Logger *logger)
 {
-    fresult = f_open(&fil, DATA_FILENAME, FA_OPEN_ALWAYS | FA_WRITE);
+    if (!logger) {
+        return LOG_FILE_OPEN_ERROR;
+    }
+    
+    fresult = f_open(&logger->fil, DATA_FILENAME, FA_OPEN_ALWAYS | FA_WRITE);
     if (fresult != FR_OK) {
         return LOG_FILE_OPEN_ERROR;
     }
 
     // Move to end of file
-    fresult = f_lseek(&fil, f_size(&fil));
+    fresult = f_lseek(&logger->fil, f_size(&logger->fil));
     if (fresult != FR_OK) {
-        f_close(&fil);
+        f_close(&logger->fil);
         return LOG_FILE_WRITE_ERROR;
     }
 
     return LOG_OK;
 }
 
-LogStatus open_data_file_read(void)
+LogStatus open_data_file_read(SD_Card_Logger *logger)
 {
-    fresult = f_open(&fil, DATA_FILENAME, FA_READ);
+    if (!logger) {
+        return LOG_FILE_READ_ERROR;
+    }
+    
+    fresult = f_open(&logger->fil, DATA_FILENAME, FA_READ);
     if (fresult != FR_OK) {
         return LOG_FILE_READ_ERROR;
     }
@@ -257,9 +289,13 @@ LogStatus open_data_file_read(void)
     return LOG_OK;
 }
 
-LogStatus close_data_file(void)
+LogStatus close_data_file(SD_Card_Logger *logger)
 {
-    fresult = f_close(&fil);
+    if (!logger) {
+        return LOG_FILE_WRITE_ERROR;
+    }
+    
+    fresult = f_close(&logger->fil);
     if (fresult != FR_OK) {
         return LOG_FILE_WRITE_ERROR;
     }
@@ -268,17 +304,29 @@ LogStatus close_data_file(void)
 }
 
 // Utility functions
-void send_uart_message(const char *message)
+void send_uart_message(SD_Card_Logger *logger, const char *message)
 {
-    HAL_UART_Transmit(&huart1, (uint8_t*)message, strlen(message), HAL_MAX_DELAY);
+    if (!logger || !logger->uart_handle || !message) {
+        return;
+    }
+    
+    HAL_UART_Transmit(logger->uart_handle, (uint8_t*)message, strlen(message), HAL_MAX_DELAY);
 }
 
-void clear_log_buffer(void)
+void clear_log_buffer(SD_Card_Logger *logger)
 {
-    memset(log_buffer, 0, sizeof(log_buffer));
+    if (!logger) {
+        return;
+    }
+    
+    memset(logger->log_buffer, 0, sizeof(logger->log_buffer));
 }
 
 uint32_t get_buffer_size(const char *buf)
 {
+    if (!buf) {
+        return 0;
+    }
+    
     return strlen(buf);
 }
